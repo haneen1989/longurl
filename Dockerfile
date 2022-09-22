dd38838ffd9f2a3e9c082ae1cabf376071d2c1f5
haneen <neno-89@live.com>
2022-09-22 09:59:27 +0300

    'update'

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM mcr.microsoft.com/windows/servercore:ltsc2022
-COPY Startup.ps1 c:/home/site/wwwroot/
-ENTRYPOINT ["powershell.exe", "c:\\home\\site\\wwwroot\\Startup.ps1"]
+COPY applicationHost.xdt C:\home\site\
+EXPOSE 8080
--- Startup.ps1
+++
@@ -1,4 +0,0 @@
-# Pull secrets path from ASPNET_SECRETS_PATH (and default to c:\secrets)
-    $secretsPath = "c:\secrets" 
-C:\home\site\wwwroot\Set-WebConfigSettings.ps1 -webConfig c:\inetpub\wwwroot\Web.config -secretsPath $secretsPath
-C:\ServiceMonitor.exe w3svc
